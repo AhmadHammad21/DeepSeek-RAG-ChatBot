6799150023c32a91c53bc41c6e7efc9808692ea5
Ahmad Hammad <ahmedhammad8765@gmail.com>
2025-03-11 22:52:16 +0000

    Chat History

--- src/retrieval/rag_pipeline.py
+++ src/retrieval/rag_pipeline.py
@@ -1,4 +1,6 @@
 from langchain_ollama import OllamaLLM
+from langchain_core.output_parsers import StrOutputParser
+
 
 class RAGPipeline:
     """Retrieves relevant chunks and generates answers using an LLM."""
@@ -16,4 +18,19 @@ class RAGPipeline:
         
         response = self.llm.invoke(prompt)
         
+        return response
+
+    def retrieve_and_generate_history(self, query, prompt_chain):
+        """Retrieves relevant chunks and generates a response."""
+        docs = self.vector_store.query(query)
+        context = "\n\n".join([doc.page_content for doc in docs])
+
+        # prompt = f"Context:\n{context}\n\nUser Query: {prompt_chain}\n\nAnswer:"
+        
+        # response = self.llm.invoke(prompt)
+        
+        # Combine the prompt chain with the retrieved context
+        full_prompt = f"{prompt_chain}\n\nContext:\n{context}"
+        processing_pipeline = self.llm | StrOutputParser()
+        response = processing_pipeline.invoke(full_prompt)
         return response
